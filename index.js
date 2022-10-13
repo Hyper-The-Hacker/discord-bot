f1094a66eb09b2c4ac6fac84c6ebc5b990e75aac
TtgNoob <108258189+TtgNoob@users.noreply.github.com>
2022-10-13 18:26:52 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js')
+const secretConfig = require('./json/secrets/config.json')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.GuildScheduledEvent,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.ThreadMember,
+        Partials.User
+    ],
+    allowedMentions: { parse: ['users'], repliedUser: true },
+})
+
+client.MessageCommands = new Collection();
+client.SlashCommands = new Collection();
+
+module.exports = client;
+
+require('./handler')(client);
+
+client.login(secretConfig.token)
