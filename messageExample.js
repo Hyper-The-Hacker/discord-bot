40099457e914e80a182bc9321176770f79e50eb8
TtgNoob <108258189+TtgNoob@users.noreply.github.com>
2022-10-13 18:28:36 +0530

    Create messageExample.js

---
+++ messageExample.js
@@ -0,0 +1,19 @@
+const { Message, Client } = require("discord.js");
+
+module.exports = {
+    name: "example",
+    aliases: ["eg.", "ex"],
+    timeout: 0,
+    clientPermissions: [],
+    userPermissions: [],
+    about: '',
+    usage: '',
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        
+    },
+};
